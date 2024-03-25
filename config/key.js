71a946c328756e7e22c9d249c862fdf7ee99707c
JagdeepSingh9697 <150673456+JagdeepSingh9697@users.noreply.github.com>
2024-03-25 18:50:35 +0530

    Add files via upload

---
+++ config/key.js
@@ -0,0 +1,4 @@
+//storing the db on mongo atlas
+module.exports = {
+    MongoURI: "mongodb+srv://jaggi761:jaggi761@cluster0.bemmqnx.mongodb.net/nodejs-authenticator?retryWrites=true&w=majority",
+}
